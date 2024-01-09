e8fe79938075ae03514382d1ecf60bbc43aed78d
Bruce-Mahagwa <96821698+Bruce-Mahagwa@users.noreply.github.com>
2024-01-09 07:24:05 +0300

    Update NavigationFooterPage.jsx

--- src/Pages/Layout/NavigationFooterPage.jsx
+++ src/Pages/Layout/NavigationFooterPage.jsx
@@ -3,14 +3,12 @@ import Footer from "../../Components/Navigation/Footer";
 import LargeScreenNav from "../../Components/Navigation/LargeScreenNav";
 import ClientLargeScreenNav from "../../Components/Navigation/ClientLargeScreenNav";
 import SmallScreenNav from "../../Components/Navigation/SmallScreenNav";
-import SmallScreenNavContent from "../../Components/Navigation/SmallScreenNavContent";
-import LoginPage from "../Login/LoginPage";
 import { UserContext } from "../../Context";
 // dependencies
 import { Outlet, Navigate } from "react-router-dom";
 import { useContext } from "react";
 const NavigationFooterPage = () => {
-  const { user, ready, loading } = useContext(UserContext);
+  const { user, ready } = useContext(UserContext);
   // console.log(loading, ready, user)
   if (user === null && ready === true) {
     return <Navigate to={"/login"} />
